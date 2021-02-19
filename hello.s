b40e06c84ed72d50305eb06ddfa521fb724f220f
Fatih Yakici <f.yakici@student.vu.nl>
2021-02-19 14:51:18 +0100

    first commit

---
+++ hello.s
@@ -0,0 +1,24 @@
+#*****************************************************************************#
+#*Program name: Assignment 1:  your first simple program *#* Description:    *#
+#*This program prints the names, study numbers and the name of the assignment*# #*on the terminal                                                            *# #*****************************************************************************#
+mystring: .asciz "Fatih Yakici 2672343\n
+		  Anass Benhaddou\n
+		  Your first simple program"
+
+
+.global main
+#***************************************************************************
+#*S u b r o u t i n e:   main                                              *
+#*D e s c r i p t i o n   :   application entry point                      *
+#***************************************************************************
+main:
+	pushq	%rbp		# store the caller's base pointer
+	movq	%rsp, %rbp	# initialize the base pointer
+	
+	movq $0, %rax		# no vector registers in use for printf
+	movq $mystring, %rdi	# load adress of a string	
+
+	mov	$0, %rdi	# load program exit code
+	call	exit		# exit the program	
+	
+	
