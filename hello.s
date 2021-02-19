f134547fae55a3a61b4df9915d3f9cc97a3f85b9
Fatih Yakici <f.yakici@student.vu.nl>
2021-02-19 15:03:33 +0100

    end: added

--- hello.s
+++ hello.s
@@ -1,9 +1,7 @@
 #*****************************************************************************#
 #*Program name: Assignment 1:  your first simple program *#* Description:    *#
 #*This program prints the names, study numbers and the name of the assignment*# #*on the terminal                                                            *# #*****************************************************************************#
-mystring: .asciz "Fatih Yakici 2672343\n
-		  Anass Benhaddou\n
-		  Your first simple program"
+mystring: .asciz "Fatih Yakici 2672343\n Anass Benhaddou\n Your first simple program"
 
 
 .global main
@@ -16,9 +14,11 @@ main:
 	movq	%rsp, %rbp	# initialize the base pointer
 	
 	movq $0, %rax		# no vector registers in use for printf
-	movq $mystring, %rdi	# load adress of a string	
-
+	movq $mystring, %rdi	# load adress of a string
+	
+end:
 	mov	$0, %rdi	# load program exit code
 	call	exit		# exit the program	
 	
 	
+
